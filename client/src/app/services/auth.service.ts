bdb48adc603dfba4822fdc4d3328a5a3f25e36b1
Borushko Dima <borushkoddima@mail.ru>
2024-08-15 16:40:18 +0300

    Display Current Login User in homepage

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -4,6 +4,7 @@ import { LoginRequest } from '../interfaces/login-request';
 import { map, Observable } from 'rxjs';
 import { AuthResponse } from '../interfaces/auth-response';
 import { HttpClient } from '@angular/common/http';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
@@ -12,18 +13,54 @@ export class AuthService {
   apiUrl: string = environment.apiUrl;
   private tokenKey = 'token'
 
-  constructor(private http:HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  login(data:LoginRequest):Observable<AuthResponse>{
-      return this.http
+  login(data: LoginRequest): Observable<AuthResponse> {
+    return this.http
       .post<AuthResponse>(`${this.apiUrl}account/login`, data)
       .pipe((
         map((response) => {
-          if(response.isSuccess){
-            localStorage.setItem(this.tokenKey , response.token)
+          if (response.isSuccess) {
+            localStorage.setItem(this.tokenKey, response.token)
           }
           return response;
         })
       ))
   }
+
+  getUserDetail = () => {
+    const token = this.getToken();
+    if (!token) return null;
+    const decodedToken: any = jwtDecode(token);
+    const userDetail = {
+      id: decodedToken.nameid,
+      fullName: decodedToken.name,
+      email: decodedToken.email,
+      roles: decodedToken.role || []
+    }
+
+    return userDetail;
+  }
+
+  isLoggedIn = (): boolean => {
+    const token = this.getToken();
+    if (!token) return false;
+    return !this.isTokenExpired();
+  }
+
+  private isTokenExpired() {
+    const token = this.getToken();
+    if (!token) return true;
+    const decoded = jwtDecode(token);
+    const isTokenExpired = Date.now() >= decoded['exp']! * 1000;
+    if (isTokenExpired) this.logout();
+    return isTokenExpired;
+  }
+
+  logout = (): void => {
+    localStorage.removeItem(this.tokenKey);
+  };
+
+  private getToken = (): string | null =>
+    localStorage.getItem(this.tokenKey) || '';
 }
