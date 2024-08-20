348061bc79a3f67c9a13e5d833a4739364239431
Borushko Dima <borushkoddima@mail.ru>
2024-08-20 13:03:11 +0300

    add AuthGuard for protection

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { AccountComponent } from './pages/account/account.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,6 @@ export const routes: Routes = [
     {
         path: 'account/:id',
         component: AccountComponent,
+        canActivate: [authGuard],
     },
 ];
