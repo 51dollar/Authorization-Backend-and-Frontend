2e918c6790cda3e818f708aab1c54a7fbc2e7a0a
Borushko Dima <borushkoddima@mail.ru>
2024-08-20 12:36:55 +0300

    add Profile Detail of current login user in detail page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { AccountComponent } from './pages/account/account.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,9 @@ export const routes: Routes = [
     {
         path: 'register',
         component: RegisterComponent,
+    },
+    {
+        path: 'account/:id',
+        component: AccountComponent,
     },
 ];
